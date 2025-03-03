0a6cd0cebda89f9bc6afb4d84938dbd2d037850c
iorg-s <svstnvgr@gmail.com>
2025-03-03 20:56:36 +0200

    Added LeafNote

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -24,3 +24,18 @@ class HTMLNode:
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+    
+
+            
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        if value is None:
+            raise ValueError("Value must be provided")
+        super().__init__(tag=tag, value=value,  props=props)
+    
+    def to_html(self):
+        if self.tag == None:
+            return self.value 
+        if self.props == None:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -29,8 +29,29 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode(props={"img src": "test.jpg", "alt": "Test Image", "width": "200", "height": "150"})
         self.assertEqual(expected_result, node.props_to_html())
     
-        
+class TestLeafNode(unittest.TestCase):
+  
+    def test_leaf_to_html(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_leaf_to_html2(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual(node.to_html(),"<p>This is a paragraph of text.</p>")
 
+    def test_leaf_to_html3(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), "<a href=\"https://www.google.com\">Click me!</a>")
+
+    def test_leaf_to_html4(self): # Tests exception
+        with self.assertRaises(ValueError) as context:
+            node = LeafNode()
+        self.assertEqual(str(context.exception), "Value must be provided")
+
+    def test_eq(self):
+        node1 = LeafNode("p", "This is a paragraph of text.")
+        node2 = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual(node1, node2)
 
 
 if __name__ == "__main__":
